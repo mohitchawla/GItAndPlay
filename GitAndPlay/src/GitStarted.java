38e43b1f26829b1d6ca0b177268feec25afa1c64
Mohit <mohitchawlacool@gmail.com>
2012-11-10 16:15:01 +0530

    Last

--- GitAndPlay/src/GitStarted.java
+++ GitAndPlay/src/GitStarted.java
@@ -7,7 +7,7 @@ public class GitStarted {
 	public static void main(String[] args) {
 		System.out.print("commited");
 		System.out.print("commited now");
-
+		System.out.print("commited now1");
 	}
 
 }
